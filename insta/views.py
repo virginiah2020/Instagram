7dd76370cbbed7df4feb9abcc7758eb3b7555eea
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-20 11:42:57 +0300

    imported redirect

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm
